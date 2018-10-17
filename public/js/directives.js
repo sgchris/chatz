9feaaa21177ebe41cf10797eb391b6bead3e163d
Gregory Chris <sgchris@gmail.com>
2018-10-17 16:19:31 +0300

    fix texts

--- public/js/directives.js
+++ public/js/directives.js
@@ -16,12 +16,12 @@ app.directive('sidebarContact', [function() {
 			'<a href="" class="list-group-item list-group-item-action" ' + 
 				'ng-click="ngClick();">type: {{contact.type}}' + 
 				// waiting for response
-				'<span class="contact-type" ng-show="contact.type == \'friend\' && !contact.approved" ' + 
+				'<span class="contact-type" ng-show="contact.type == \'friend\' && !contact.approved && !contact.responded" ' + 
 					'title="{{contact.name}} (email {{contact.email}}) still did not respond to your friend request" '+
 					'ng-class="{\'contact-pending\': contact.approved == 0}">Waiting for response</span>' + 
 				// Pending frield request
-				'<span class="contact-type" ng-show="contact.type == \'follower\' && !contact.approved" ' + 
-					'title="{{contact.name}} (email {{contact.email}}) wants to add you to his contacts" '+
+				'<span class="contact-type" ng-show="contact.type == \'follower\' && !contact.approved && !contact.responded" ' + 
+					'title="{{contact.name}} (email {{contact.email}}) wants to contact with you" '+
 					'ng-class="{\'contact-pending\': contact.approved == 0}">Approve friend request</span>' + 
 				'<h5 class="mb-1">{{ contact.name }}</h5>' + 
 				'<p class="mb-1">{{ contact.email }}</p>' + 
