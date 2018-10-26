3603f26477153b8bba14f37a13cc542f1ff744cd
Gregory Chris <sgchris@gmail.com>
2018-10-26 10:48:51 +0300

    updated seeder for relations (added my user)

--- database/seeds/RelationsTableSeeder.php
+++ database/seeds/RelationsTableSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+
 class RelationsTableSeeder extends Seeder
 {
     /**
@@ -13,13 +15,13 @@ class RelationsTableSeeder extends Seeder
     {
 		DB::table('relations')->truncate();
         
-		$users = \App\User::all()->pluck('id');
+		$users = User::all()->pluck('id');
         $pairs = [];
         for ($i=0; $i<30; ++$i) {
 
 			// select 2 users
-            $userId = $users[mt_rand(0, count($users) - 1)];
-            $user2Id = $users[mt_rand(0, count($users) - 1)];
+            $userId = $users[mt_rand(1, count($users) - 1)];
+            $user2Id = $users[mt_rand(1, count($users) - 1)];
 			if ($userId == $user2Id) continue;
 
             $pairName = $userId.'_'.$user2Id;
@@ -38,5 +40,48 @@ class RelationsTableSeeder extends Seeder
                 ]);
             }
         } 
+
+
+		// generate for my user
+		$this->addForMyUser();
     }
+
+	// generate friends for 'sgchris' user (my test user);
+	protected function addForMyUser()
+	{
+		$userIds = User::where('id', '!=', 1)->get()->pluck('id')->all();
+		$usersUsed = [];
+
+		// add friends
+		for ($i=0; $i<4; ++$i) {
+			$userId = $userIds[mt_rand(0, count($userIds) - 1)];
+			if (!in_array($userId, $usersUsed)) { 
+				$usersUsed[] = $userId;
+
+				echo "adding friend ".(User::find($userId)->name)." ({$userId})\n";
+				DB::table('relations')->insert([
+					'user_id' => 1,
+					'friend_id' => $userId,
+					'updated_at' => \Carbon\Carbon::now(),
+					'created_at' => \Carbon\Carbon::now(),
+				]);
+			}
+		}
+
+		// add followers
+		for ($i=0; $i<4; ++$i) {
+			$userId = $userIds[mt_rand(0, count($userIds) - 1)];
+			if (!in_array($userId, $usersUsed)) { 
+				$usersUsed[] = $userId;
+
+				echo "adding follower ".(User::find($userId)->name)." ({$userId})\n";
+				DB::table('relations')->insert([
+					'user_id' => $userId,
+					'friend_id' => 1,
+					'updated_at' => \Carbon\Carbon::now(),
+					'created_at' => \Carbon\Carbon::now(),
+				]);
+			}
+		}
+	}
 }
